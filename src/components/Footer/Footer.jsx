20f377023064e0fa3a97313ed9423b82eaca5f04
CBBS920 <tech-kawane@cbbs.jp>
2024-05-21 17:23:21 +0900

    ディレクトリ追加

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FooterMenu } from './FooterMenu/FooterMenu';
+
+export const Footer = () => {
+  return (
+    <>
+        <div>フッター</div>
+        <FooterMenu />
+    </>
+  );
+};
